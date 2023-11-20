086e1e4d91d9eab1d18b5c1fb7431cce8e1f8076
pravallikaraj52 <142988822+bpravalikaraj52@users.noreply.github.com>
2023-11-20 10:55:59 +0530

    update wallmartDB

---
+++ changes/2.0/script/2_request_1.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+CREATE TABLE truck (
+  brand VARCHAR(255),
+  model VARCHAR(255),
+  year INT
+);
---
+++ changes/3.0/script/2_request_1.sql
@@ -0,0 +1,7 @@
+--liquibase formatted sql
+
+CREATE TABLE lorry (
+  brand VARCHAR(255),
+  model VARCHAR(255),
+  year INT
+);
